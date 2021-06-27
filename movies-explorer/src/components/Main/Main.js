27438ab323ac5fe8fe237b84aa6ce2d9024b7e22
Surovev <dmitriysurovev@gmail.com>
2021-06-27 21:59:53 +0300

    first reviev

--- movies-explorer/src/components/Main/Main.js
+++ movies-explorer/src/components/Main/Main.js
@@ -3,6 +3,7 @@ import AboutProject from '../AboutProject/AboutProject.js';
 import Techs from '../Techs/Techs.js';
 import AboutMe from '../AboutMe/AboutMe.js';
 import Portfolio from '../Portfolio/Portfolio.js';
+import Footer from '../Footer/Footer.js';
 
 function Main () {
   return (
@@ -12,6 +13,7 @@ function Main () {
       <Techs />
       <AboutMe />
       <Portfolio />
+      <Footer />
     </div>
   );
 }
